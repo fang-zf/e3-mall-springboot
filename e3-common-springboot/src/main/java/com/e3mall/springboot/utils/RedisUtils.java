6da9a5e4e4b0db77f37c7b7f90c36ff9e038d56f
fang-zf <fzf_art@163.com>
2020-01-01 16:37:46 +0800

    优化 RedisUtils工具类

--- e3-common-springboot/src/main/java/com/e3mall/springboot/utils/RedisUtils.java
+++ e3-common-springboot/src/main/java/com/e3mall/springboot/utils/RedisUtils.java
@@ -45,9 +45,7 @@ public class RedisUtils {
 
     /**
      * 根据key 获取过期时间
-     * 
-     * @param key
-     *            键 不能为null
+     * @param key 键 不能为null
      * @return 时间(秒) 返回0代表为永久有效
      */
     public long getExpire(String key) {
@@ -56,9 +54,7 @@ public class RedisUtils {
 
     /**
      * 判断key是否存在
-     * 
      * @param key
-     *            键
      * @return true 存在 false不存在
      */
     public boolean hasKey(String key) {
@@ -71,10 +67,8 @@ public class RedisUtils {
     }
 
     /**
-     * 删除缓存
-     * 
-     * @param key
-     *            可以传一个值 或多个
+     * 删除缓存 
+     * @param key 可以传一个值 或多个
      */
     @SuppressWarnings("unchecked")
     public void del(String... key) {
@@ -165,11 +159,8 @@ public class RedisUtils {
     // ================================Map=================================
     /**
      * HashGet
-     * 
-     * @param key
-     *            键 不能为null
-     * @param item
-     *            项 不能为null
+     * @param key 键 不能为null
+     * @param item 项 不能为null
      * @return 值
      */
     public Object hget(String key, String item) {
