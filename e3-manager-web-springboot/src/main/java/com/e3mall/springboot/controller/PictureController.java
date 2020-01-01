18685aa4e774643b6ad08f40c3e88fda4fe3d969
fang-zf <fzf_art@163.com>
2020-01-01 16:07:31 +0800

    删除FastDFSClient旧版本中的冗余代码

--- e3-manager-web-springboot/src/main/java/com/e3mall/springboot/controller/PictureController.java
+++ e3-manager-web-springboot/src/main/java/com/e3mall/springboot/controller/PictureController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.alibaba.fastjson.JSONObject;
 import com.e3mall.springboot.config.FileUploadProperteis;
 import com.e3mall.springboot.utils.FastDFSClient;
 import com.e3mall.springboot.utils.JsonUtils;
@@ -31,37 +30,7 @@ public class PictureController {
 	private FastDFSClient fastDFSClient;
 	@Autowired
 	private FileUploadProperteis fileUploadProperteis;
-	
-	/*@Value("${IMAGE_SERVER_URL}")
-	private String IMAGE_SERVER_URL;
 
-	@RequestMapping(value="/pic/upload", produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
-	@ResponseBody
-	public String uploadFile(MultipartFile uploadFile) {
-		try {
-			//把图片上传的图片服务器
-			FastDFSClient01 fastDFSClient = new FastDFSClient01("classpath:conf/client.conf");
-			//取文件扩展名
-			String originalFilename = uploadFile.getOriginalFilename();
-			String extName = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
-			//得到一个图片的地址和文件名
-			String url = fastDFSClient.uploadFile(uploadFile.getBytes(), extName);
-			//补充为完整的url
-			url = IMAGE_SERVER_URL + url;
-			//封装到map中返回
-			Map result = new HashMap<>();
-			result.put("error", 0);
-			result.put("url", url);
-			return JsonUtils.objectToJson(result);
-		} catch (Exception e) {
-			e.printStackTrace();
-			Map result = new HashMap<>();
-			result.put("error", 1);
-			result.put("message", "图片上传失败");
-			return JsonUtils.objectToJson(result);
-		}
-	}*/
-	
 	@RequestMapping(value="/pic/upload", produces=MediaType.TEXT_PLAIN_VALUE+";charset=utf-8")
 	@ResponseBody
 	public String uploadFile(MultipartFile uploadFile) {
